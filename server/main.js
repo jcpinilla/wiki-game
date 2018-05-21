96d78583b580cd5391436bdd23785d4909708ddc
jm-dominguez <jm.dominguez@uniandes.edu.co>
2018-05-21 15:00:52 -0500

    Problema de accesibilidad
    Tienen un problema de accesibilidad, puesto que el html no tiene atributo lang. Esto se puede solucionar en el servidor de meteor durante el startup.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
 import { Meteor } from "meteor/meteor";
 import "../imports/api/games.js";
 import "../imports/api/wiki.js";
+/*
+Tienen un problema de accesibilidad, puesto que el html no tiene atributo lang. En el servidor de meteor es posible inyectar código en el html
+para evitar este problema
+*/
 
 Meteor.startup(() => {
 	// code to run on server at startup
